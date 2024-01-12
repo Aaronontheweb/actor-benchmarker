ab785012c97177f3c80ef9d9efd7beff75fab14d
Aaron Stannard <aaron@petabridge.com>
2024-01-12 13:22:48 -0600

    added standard template

---
+++ src/Akka.Benchmarker.App/TransformerActor.cs
@@ -0,0 +1,12 @@
+﻿namespace Akka.Benchmarker.App;
+
+public class TransformerActor : ReceiveActor
+{
+    public TransformerActor()
+    {
+        Receive<string>(str =>
+        {
+            Sender.Tell(str.ToUpperInvariant());
+        });
+    }
+}
